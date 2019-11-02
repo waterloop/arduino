cf49dae9a4db8486fa38aaf8d570d074cc03abf4
Waterloop <teamwaterloop@gmail.com>
2019-11-02 13:10:49 -0400

    added instance id for each function

--- src/packetcodec.cpp
+++ src/packetcodec.cpp
@@ -1,49 +1,18 @@
 #include <stdint.h>
 
-#define IMU_1_ACCEL_ID_1 1024
-#define IMU_1_GYRO_ID_1 1025
-
-#define IMU_1_ACCEL_ID_2 1026
-#define IMU_1_GYRO_ID_2 1027
-
-#define TEMP_1_ID_1 1028
-#define TEMP_1_ID_2 1029
-#define TEMP_1_ID_3 1030
-#define TEMP_1_ID_4 1031
-#define TEMP_1_ID_5 1032
-#define TEMP_1_ID_6 1033
-#define TEMP_1_ID_7 1034
-#define TEMP_1_ID_8 1035
-#define TEMP_1_ID_9 1036
-#define TEMP_1_ID_10 1037
-
-#define TEMP_2_ID_1 1038
-#define TEMP_2_ID_2 1039
-
-#define PHOTO_2_ID_1 1040
-#define PHOTO_2_ID_2 1041
-
-#define HALL_1_ID_1 1042
-#define HALL_1_ID_2 1043
-#define HALL_1_ID_3 1044
-#define HALL_1_ID_4 1045
-#define HALL_1_ID_5 1046
-#define HALL_1_ID_6 1047
-
-#define PIEZO_1_ID_1 1048
-#define PIEZO_1_ID_2 1049
-
-const int IMU_ACCEL_ID[] = {IMU_1_ACCEL_ID_1, IMU_1_ACCEL_ID_2};
-const int IMU_GYRO_ID[] = {IMU_1_GYRO_ID_1, IMU_1_GYRO_ID_2};
-const int TEMP_1_ID[] = {TEMP_1_ID_1, TEMP_1_ID_2, TEMP_1_ID_3, TEMP_1_ID_4, TEMP_1_ID_5, TEMP_1_ID_6, TEMP_1_ID_7, TEMP_1_ID_8, TEMP_1_ID_9, TEMP_1_ID_10};
-const int TEMP_2_ID[] = {TEMP_2_ID_1, TEMP_2_ID_2};
-const int PHOTO_2_ID[] = {PHOTO_2_ID_1, PHOTO_2_ID_2};
-const int HALL_1_ID[] = {HALL_1_ID_1, HALL_1_ID_2, HALL_1_ID_3, HALL_1_ID_4, HALL_1_ID_5, HALL_1_ID_6};
-const int PIEZO_1_ID[] = {PIEZO_1_ID_1, PIEZO_1_ID_2};
+const int IMU_1_ACCEL_ID[] = {1024, 1026};
+const int IMU_1_GYRO_ID[] = {1025, 1027};
+const int TEMP_1_ID[] = {1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037};
+const int TEMP_2_ID[] = {1038, 1039};
+const int PHOTO_2_ID[] = {1040, 1041};
+const int HALL_1_ID[] = {1042, 1043, 1044, 1045, 1046, 1047};
+const int PIEZO_1_ID[] = {1048, 1049};
 
 void send_can(uint32_t packet_id, uint8_t len, uint8_t *data);
 
-void serialize_mcp6050_accelerometer(int16_t x, int16_t y, int16_t z) {
+// serialize data 
+
+void serialize_mcp6050_accelerometer(int8_t instance, int16_t x, int16_t y, int16_t z) {
     uint8_t data[6];
 
     // flip the indexes since it takes the last 8 bits each time 
@@ -57,10 +26,10 @@ void serialize_mcp6050_accelerometer(int16_t x, int16_t y, int16_t z) {
     data[5] = z;
     data[4] = z >> 8;
 
-    send_can(IMU_1_ACCEL_ID_1, 6, data);
+    send_can(IMU_1_ACCEL_ID[instance], 6, data);
 }
 
-void serialize_mcp6050_gyroscope(int16_t x, int16_t y, int16_t z) {
+void serialize_mcp6050_gyroscope(int8_t instance, int16_t x, int16_t y, int16_t z) {
     uint8_t data[6];
 
     data[1] = x;
@@ -72,28 +41,28 @@ void serialize_mcp6050_gyroscope(int16_t x, int16_t y, int16_t z) {
     data[5] = z;
     data[4] = z >> 8;
 
-    send_can(IMU_1_GYRO_ID_1, 6, data);
+    send_can(IMU_1_GYRO_ID[instance], 6, data);
 }
 
-void serialize_tmp411(int16_t t) {
+void serialize_tmp411(int8_t instance, int16_t t) {
     uint8_t data[2];
 
     data[1] = t;
     data[0] = t >> 8;
 
-    send_can(TEMP_1_ID_1, 2, data);
+    send_can(TEMP_1_ID[instance], 2, data);
 }
 
-void serialize_temp2(int16_t t) {
+void serialize_temp2(int8_t instance, int16_t t) {
     uint8_t data[2];
 
     data[1] = t;
     data[0] = t >> 8;
 
-    send_can(TEMP_1_ID_1, 2, data);
+    send_can(TEMP_1_ID[instance], 2, data);
 }
 
-void serialize_photo2(int32_t c) {
+void serialize_photo2(int8_t instance, int32_t c) {
     uint8_t data[4];
 
     data[3] = c;
@@ -101,23 +70,23 @@ void serialize_photo2(int32_t c) {
     data[1] = c >> 16;
     data[0] = c >> 24;
 
-    send_can(PHOTO_2_ID_1, 4, data);
+    send_can(PHOTO_2_ID[instance], 4, data);
 }
 
-void serialize_hall1(int16_t f) {
+void serialize_hall1(int8_t instance, int16_t f) {
     uint8_t data[2];
 
     data[1] = f;
     data[0] = f >> 8;
 
-    send_can(HALL_1_ID_1, 2, data);
+    send_can(HALL_1_ID[instance], 2, data);
 }
 
-void serialize_piezo1(int16_t v) {
+void serialize_piezo1(int8_t instance, int16_t v) {
     uint8_t data[2];
 
     data[1] = v;
     data[0] = v >> 8;
 
-    send_can(PIEZO_1_ID_1, 2, data);
+    send_can(PIEZO_1_ID[instance], 2, data);
 }
